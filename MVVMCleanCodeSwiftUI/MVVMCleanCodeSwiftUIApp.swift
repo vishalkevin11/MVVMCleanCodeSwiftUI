24a337e11cead541b4a2a6b3f93a5047d2a1aac1
Kevin Vishal <vishalkevin11@gmail.com>
2023-02-12 14:07:18 +0530

    BASE PROJECT CODE

---
+++ MVVMCleanCodeSwiftUI/MVVMCleanCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMCleanCodeSwiftUIApp.swift
+//  MVVMCleanCodeSwiftUI
+//
+//  Created by kevin vishal on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMCleanCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
